094a02e0419fa24b3319d903377a59663561fe2e
David Mayer <dmayer2@gmail.com>
2019-10-17 19:38:14 -0600

    Order of operations - ensure packages are installed in order of dependencies

--- lib/initialize.R
+++ lib/initialize.R
@@ -4,6 +4,15 @@ options(repos=structure(c(CRAN="http://cloud.r-project.org/")))
 # Check that Packages are installed
 message('Checking available packages, installing those that are missing...')
 source('lib/check_packages.R')
+source('lib/bioc_check_packages.R')
+
+# Install BiocManager (if not already installed), necessary to install 'rhdf5' a BioRad Dependency.
+check_packages(pkg = 'BiocManager')
+
+# Install 'rhdf5' (if not already installed)
+bioc_check_packages(pkg = 'rhdf5')
+
+# Install other necessary packages
 check_packages(pkg = list('shiny',
                           'flexdashboard',
                           'tidyverse',
